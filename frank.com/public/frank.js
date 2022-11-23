6ebc1c21c67a9f8019860ad1c3b57a6cbcd4d40c
FrankLai <laihongjiang@foxmail.com>
2022-11-23 14:52:57 +0800

    init

---
+++ frank.com/public/frank.js
@@ -0,0 +1,25 @@
+// const request = new XMLHttpRequest()
+// request.open('GET','http://127.0.0.1:8889/friends.json')
+// request.onreadystatechange = () => {
+//   if(request.readyState === 4 && request.status === 200){
+//     console.log(request.response)
+//   }
+// }
+
+// request.send()
+
+
+function jsonp(url){
+  return new Promise((resolve,reject) => {
+    const random = "frankJSONPCallbackName" + Math.random();
+    window[random] = data => resolve(data);
+    const script = document.createElement("script");
+    script.src = `${url}?callback=${random}`;
+    script.onload = () => script.remove();
+    script.onerror = () => reject();
+    document.body.appendChild(script);
+  })
+}
+
+jsonp('http://localhost:8889/friends.js').then((data)=>console.log(data),()=>{})
+
